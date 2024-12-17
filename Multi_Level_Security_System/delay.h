d9033b90f4a40417174e44036629cb93dc51a3a7
Pentagon360 <149203278+Pentagon360@users.noreply.github.com>
2024-12-17 10:33:53 +0530

    Update delay.h

--- Multi_Level_Security_System/delay.h
+++ Multi_Level_Security_System/delay.h
@@ -1,1 +1,25 @@
-*
+//delay.h
+void delay_ms(unsigned int d)
+{
+	T0PR = 15000-1;
+	T0TCR = 0X01;
+	while(T0TC<d);
+	T0TCR = 0X03;
+	T0TCR = 0X00;
+}
+void delay_sec(unsigned int d)
+{
+	T0PR = 15000000-1;
+	T0TCR = 0X01;
+	while(T0TC<d);
+	T0TCR = 0X03;
+	T0TCR = 0X00;
+}
+void delay_micro(unsigned int d)
+{
+	T0PR = 15-1;
+	T0TCR = 0X01;
+	while(T0TC<d);
+	T0TCR = 0X03;
+	T0TCR = 0X00;
+}
